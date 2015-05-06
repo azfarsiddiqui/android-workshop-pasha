484d67d19f9ead8a4e29a99e652fadaa91e25b14
Muhammad Azfar Siddiqui <azfar.siddiqui@tenpearls.com>
2015-05-06 19:18:00 +0500

    Wrapped up the sample news displaying application

--- news-app/app/src/main/java/com/tenpearls/android/newsapp/MainActivity.java
+++ news-app/app/src/main/java/com/tenpearls/android/newsapp/MainActivity.java
@@ -1,39 +1,156 @@
 package com.tenpearls.android.newsapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    ListView mListView;
+    ArrayList<News> mNewsItems = new ArrayList<>();
+    RequestQueue mRequestQueue;
+    ImageLoader mImageLoader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        initUI();
+        initVolley();
+        dispatchNetworkRequestToGetNews();
+    }
+
+    private void initUI() {
+
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.listViewNews);
     }
 
+    private void initVolley() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        mRequestQueue = Volley.newRequestQueue(this);
+        mImageLoader = new ImageLoader(mRequestQueue, new LruBitmapCache(LruBitmapCache.getCacheSize(this)));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void dispatchNetworkRequestToGetNews () {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        String url = "https://api.myjson.com/bins/2y7ol";
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Loading...");
+        progressDialog.show();
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String s) {
+
+                    parseNewsJson(s);
+                    updateUI();
+                    progressDialog.hide();
+                }
+            },
+            new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+
+                    progressDialog.hide();
+                    Toast.makeText(MainActivity.this, volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        );
+
+        mRequestQueue.add(request);
+    }
+
+    private void parseNewsJson(String json) {
+
+        try {
+            JSONArray jsonArray = new JSONArray(json);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                News newsItem = new News();
+                newsItem.setTitle(jsonObject.getString("title"));
+                newsItem.setDescription(jsonObject.getString("abstract"));
+                newsItem.setThumbnailUrl(jsonObject.getString("url"));
+
+                mNewsItems.add(newsItem);
+            }
+        }
+        catch (Exception exception) {
+            Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updateUI() {
+
+        NewsAdapter newsAdapter = new NewsAdapter();
+        mListView.setAdapter(newsAdapter);
+    }
+
+    private class NewsAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+
+            return mNewsItems.size();
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public Object getItem(int position) {
+
+            return mNewsItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            News newsItem = (News) getItem(position);
+
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.list_item_news, parent, false);
+            }
+
+            TextView txtTitle = (TextView) convertView.findViewById(R.id.txtTitle);
+            TextView txtDescription = (TextView) convertView.findViewById(R.id.txtDescription);
+            NetworkImageView imageView = (NetworkImageView) convertView.findViewById(R.id.imageThumbnail);
+
+            txtTitle.setText(newsItem.getTitle());
+            txtDescription.setText(newsItem.getDescription());
+            imageView.setImageUrl(newsItem.getThumbnailUrl(), mImageLoader);
+
+            return convertView;
+        }
     }
 }
---
+++ news-app/app/src/main/java/com/tenpearls/android/newsapp/News.java
@@ -0,0 +1,35 @@
+package com.tenpearls.android.newsapp;
+
+/**
+ * Created by azfar on 5/6/15.
+ */
+public class News {
+
+    private String title;
+    private String description;
+    private String thumbnailUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
