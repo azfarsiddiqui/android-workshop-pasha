cb98ade4e3af38fa475869f2bccef6c044e0c188
Muhammad Azfar Siddiqui <azfar.siddiqui@tenpearls.com>
2015-05-06 17:52:24 +0500

    Completed datastorage & volley samples

--- data-storage/app/src/main/java/com/tenpearls/android/datastorage/MainActivity.java
+++ data-storage/app/src/main/java/com/tenpearls/android/datastorage/MainActivity.java
@@ -1,18 +1,25 @@
 package com.tenpearls.android.datastorage;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.File;
+import java.io.FileOutputStream;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     Button mBtnSave;
     Button mBtnRetrieve;
+    Button mBtnWriteFile;
+    Button mBtnDeleteFile;
     EditText mTxtName;
     final String SHARED_PREF_KEY = "name";
 
@@ -31,26 +38,38 @@ public class MainActivity extends ActionBarActivity {
 
         mBtnSave = (Button) findViewById(R.id.btnSave);
         mBtnRetrieve = (Button) findViewById(R.id.btnRetrieve);
+        mBtnWriteFile = (Button) findViewById(R.id.btnWriteFile);
+        mBtnDeleteFile = (Button) findViewById(R.id.btnDeleteFile);
         mTxtName = (EditText) findViewById(R.id.txtName);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btnSave:
+                onSave();
+                break;
+            case R.id.btnRetrieve:
+                onRetrieve();
+                break;
+            case R.id.btnWriteFile:
+                onWriteFile();
+                break;
+            case R.id.btnDeleteFile:
+                onDeleteFile();
+                break;
+            default:
+                Log.e("OnClick", "Unhandled case..");
+        }
     }
 
     private void setListeners() {
 
-        mBtnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                onSave();
-            }
-        });
-
-        mBtnRetrieve.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                onRetrieve();
-            }
-        });
+        mBtnSave.setOnClickListener(this);
+        mBtnRetrieve.setOnClickListener(this);
+        mBtnWriteFile.setOnClickListener(this);
+        mBtnDeleteFile.setOnClickListener(this);
     }
 
     private void onSave() {
@@ -78,4 +97,27 @@ public class MainActivity extends ActionBarActivity {
         editor.putString (SHARED_PREF_KEY, name);
         editor.commit ();
     }
+
+    private void onWriteFile() {
+
+        String filename = "hello.txt";
+        String string = "This is my content";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void onDeleteFile() {
+
+        File dir = getFilesDir();
+        File file = new File(dir, "hello.txt");
+        boolean deleted = file.delete();
+    }
+
 }
--- volley/app/src/main/java/com/tenpearls/android/volley/MainActivity.java
+++ volley/app/src/main/java/com/tenpearls/android/volley/MainActivity.java
@@ -1,39 +1,71 @@
 package com.tenpearls.android.volley;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    RequestQueue mRequestQueue;
+    Button mBtnDispatchNetworkRequest;
+    TextView mTxtNetworkResponse;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        initUI();
+    }
+
+    private void initUI() {
+
         setContentView(R.layout.activity_main);
+
+        mBtnDispatchNetworkRequest = (Button) findViewById(R.id.btnDispatchNetworkRequest);
+        mTxtNetworkResponse = (TextView) findViewById(R.id.txtNetworkResponse);
+
+        mBtnDispatchNetworkRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchNetworkRequestToGetNews();
+            }
+        });
     }
 
+    private void dispatchNetworkRequestToGetNews () {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        String url = "https://api.myjson.com/bins/4wq9h";
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String s) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+                    mTxtNetworkResponse.setText(s);
+                }
+            },
+            new Response.ErrorListener() {
 
-        return super.onOptionsItemSelected(item);
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+
+                    Toast.makeText(MainActivity.this, volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        );
+
+        mRequestQueue = Volley.newRequestQueue(this);
+        mRequestQueue.add(request);
     }
 }
